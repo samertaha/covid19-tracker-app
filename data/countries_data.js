c9c965781ce9f2e5b53de9cafed5ec81280c1b9f
Samer taha <alrazy1@gmail.com>
2022-07-13 21:20:20 +0300

    changes cors proxy to https://cors-anywhere.herokuapp.com/ in both api's

--- data/countries_data.js
+++ data/countries_data.js
@@ -10692,7 +10692,7 @@
 
 async function fetchData(url) {
   try {
-    const res = await fetch('https://thingproxy.freeboard.io/fetch/' + url);
+    const res = await fetch('https://cors-anywhere.herokuapp.com/' + url);
     const data = await res.json();
     return data;
   } catch (e) {
